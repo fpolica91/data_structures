7351b6971d04ec4f197f6d62f0a53f28ba43b9f0
Fabricio Policarpo <fabriciopolicarpo0@gmail.com>
2020-12-05 21:53:42 -0500

    Initial Commit

---
+++ data_sturtures/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  data_sturtures
+//
+//  Created by fabricio policarpo on 12/5/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
